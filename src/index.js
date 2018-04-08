756ee4a8f8a78716e44b08de363355f7316f6ba0
tim <tim@glo.id.au>
2018-04-09 09:06:44 +1000

    tidy up index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
 import fs from 'fs';
 import MissionControl from './mission-control';
 
-// Some test string data that's useful for debugging.
-// import {
-//   startCommands,
-//   brokenCommands,
-//   solarRadiationCorruptedCommands,
-//   testForCollisionCommands,
-//   twoRoversOneCommand,
-//   invalidGrid,
-// } from './data/test-data';
+/*
+// Various sets of test string data that are useful for logic debugging.
+import {
+  startCommands,
+  brokenCommands,
+  solarRadiationCorruptedCommands,
+  testForCollisionCommands,
+  twoRoversOneCommand,
+  invalidGrid,
+} from './data/test-data';
+*/
 
-const inputData = fs.readFile(process.env.FILENAME, 'utf8', (err, contents) => {
-    const housten = new MissionControl({
-      commandData: contents,
-      location: 'Housten',
-    });
+fs.readFile(process.env.FILENAME, 'utf8', (err, contents) => {
+  const housten = new MissionControl({
+    commandData: contents,
+    location: 'Housten',
+  });
 
-    // Begin the deployment of rovers at will!
-    housten.deployRovers();
+  // Begin the deployment of rovers at will!
+  housten.deployRovers();
 });
 
 

4b98f2e493b544949a829f1eb11f65a15a408807
tim <tim@glo.id.au>
2018-04-08 18:56:26 +1000

    take data in the form of a bash arguement.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
+import fs from 'fs';
 import MissionControl from './mission-control';
-import {
-  startCommands,
-  brokenCommands,
-  solarRadiationCorruptedCommands,
-  testForCollisionCommands,
-  twoRoversOneCommand,
-  invalidGrid,
-} from './data/test-data';
 
-const housten = new MissionControl({
-  commandData: brokenCommands,
-  location: 'Housten',
+// Some test string data that's useful for debugging.
+// import {
+//   startCommands,
+//   brokenCommands,
+//   solarRadiationCorruptedCommands,
+//   testForCollisionCommands,
+//   twoRoversOneCommand,
+//   invalidGrid,
+// } from './data/test-data';
+
+const inputData = fs.readFile(process.env.FILENAME, 'utf8', (err, contents) => {
+    const housten = new MissionControl({
+      commandData: contents,
+      location: 'Housten',
+    });
+
+    // Begin the deployment of rovers at will!
+    housten.deployRovers();
 });
 
-// Begin the deployment of rovers at will!
-housten.deployRovers();
+
 
---
+++ test/tests/mission-control.tests.js
@@ -0,0 +1,6 @@
+import MissionControl from '../../src/mission-control';
+
+test('', () => {
+  // expect(sum(1, 2)).toBe(3);
+
+});
